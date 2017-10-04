18e3ed44b0fcf6aa0c015835db028fc8b5555da7
Ian <ianburrows.sa@gmail.com>
2017-10-04 15:24:06 +0200

    Added converter and SampleManager

---
+++ CodingDojo03/Model/OnStockToColorBrushConverter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace CodingDojo03.Model
+{
+    public class OnStockToColorBrushConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int temp = (int)value;
+            if (temp < 10)
+            {
+                return new SolidColorBrush(Colors.Red);
+            }
+            else if (temp >= 10 && temp < 20)
+            {
+                return new SolidColorBrush(Colors.Orange);
+            }
+            else
+            {
+                return new SolidColorBrush(Colors.Green);
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    #endregion
+}
---
+++ CodingDojo03/Model/SoftwarePackage.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodingDojo03.Model
+{
+    class SoftwarePackage
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public double SalesPrice { get; set; }
+        public double PurchasePrice { get; set; }
+        public int OnStock { get; set; }
+
+        public SoftwarePackage(string name, string group, double salesPrice, double purchasePrice, int onStock)
+        {
+            this.Name = name;
+            this.Group = group;
+            this.SalesPrice = salesPrice;
+            this.PurchasePrice = purchasePrice;
+            this.OnStock = onStock;
+        }
+
+        
+    }
+}
